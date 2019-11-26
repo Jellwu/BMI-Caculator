371d4443f48891dbb619a9c04ff29b6015805197
Jell <jell9916@gmail.com>
2019-11-26 18:19:16 +0800

    V2-改用case與修正resultbtn

--- js/BMI.js
+++ js/BMI.js
@@ -1,48 +1,56 @@
 //DOM宣告
-var hnum = document.querySelector('.height');
-var wnum = document.querySelector('.weight');
-var resultbtn = document.querySelector('.result');
-var recordlist = document.querySelector('.recorder');
+let hnum = document.querySelector('.height');
+let wnum = document.querySelector('.weight');
+let resultbtn = document.querySelector('.result');
+let recordlist = document.querySelector('.recorder');
 //宣告local取到的字串轉陣列放到data
-var data = JSON.parse(localStorage.getItem('Usernum')) || []
-var newdt = new Date();
+let data = JSON.parse(localStorage.getItem('Usernum')) || []
+let newdt = new Date();
 updatepage();
 
 //函式庫
 //1.計算BMI並把等級與值塞到陣列
 function insertdata(){
-  var height = parseInt(hnum.value);
-  var weight = parseInt(wnum.value);
-  var BMI = (weight) / ((height/100) * (height/100));
-  var showBMI = (Math.round(BMI*100)/100);
-  var level="";
+  let height = parseInt(hnum.value);
+  let weight = parseInt(wnum.value);
+  let BMI = (weight) / ((height/100) * (height/100));
+  let showBMI = (Math.round(BMI*100)/100);
+  let level="";
 
-  var fulldate = ''+ newdt.getFullYear() +'-'+ parseInt(newdt.getMonth()+1) +'-' + newdt.getDate() +''
-  if(BMI < 18.5){
+  let fulldate = ''+ newdt.getFullYear() +'-'+ parseInt(newdt.getMonth()+1) +'-' + newdt.getDate() +''
+
+  switch(true){
+    case(showBMI < 18.5):
     level="過輕";
     data.push({height,weight,showBMI,level,fulldate});
-  }else if (showBMI >= 18.5 && showBMI < 24){
+    break;
+    case(showBMI >= 18.5 && showBMI < 24):
     level="理想";
     data.push({height,weight,showBMI,level,fulldate});
-  }else if (showBMI >= 24 && showBMI < 27){
+    break;
+    case(showBMI >= 24 && showBMI < 27):
     level="肥胖";
     data.push({height,weight,showBMI,level,fulldate});
-  }else if (showBMI >= 27 && showBMI < 32){
+    break;
+    case(showBMI >= 27 && showBMI < 32):
     level="中度肥胖";
     data.push({height,weight,showBMI,level,fulldate});
-  }else if (showBMI >= 32){
+    break;
+    case(showBMI >= 32):
     level="過度肥胖";
     data.push({height,weight,showBMI,level,fulldate});
+    break;
   }
   updatepage();
   localStorage.setItem('Usernum',JSON.stringify(data));
+  //完成後呼叫refreshbtn函式修改樣式
+  refreshbtn();
 }
 
 //2.字串串接函式
 function joinstr(){
-  var str ="";
-  var str2 =""
-  for (var i = 0; i < data.length; i++) {
+  let str ="";
+  for (let i = 0; i < data.length; i++) {
     //累加html
     str += '<li data-num="'+ (i+1) +'"><span class="Listtitle">'+ data[i].level+'</span><span><small>BMI</small>'
     + data[i].showBMI +'</span><span><small>weight</small>'+ data[i].weight +'KG</span><span><small>height</small>'
@@ -55,10 +63,10 @@ function joinstr(){
 function updatepage(){
   recordlist.innerHTML = joinstr();
   //宣告欲修改樣式的DOM
-  var titlecolor = document.querySelectorAll('.Listtitle');
-  var liborder = document.querySelectorAll('.recorder li');
+  let titlecolor = document.querySelectorAll('.Listtitle');
+  let liborder = document.querySelectorAll('.recorder li');
   //修改樣式迴圈(抓.Listtile的陣列數量)
-  for (var i = 0; i < titlecolor.length; i++) {
+  for (let i = 0; i < titlecolor.length; i++) {
     switch(titlecolor[i].textContent){
      case "過輕":
      titlecolor[i].style.color='#6C93DB';
@@ -87,28 +95,26 @@ function updatepage(){
   }
 
 //4.按鈕更新函式:點下btn後改寫btn樣式與div內容
-  function refreshbtn(e){
+  function refreshbtn(){
     //宣告要用到的DOM元素
-    var resultbtn = document.querySelector('.result');
-    var resulttext = document.querySelector('.condition');
-    var baninput = document.querySelector('input')
-    var inputimg = document.querySelector('.circlepic');
+    let resulttext = document.querySelector('.condition');
+    let baninput = document.querySelector('input')
+    let inputimg = document.querySelector('.circlepic');
     //宣告變數
-    var Len = data.length;
-    var btndata = [];
+    const Len = data.length;
+    let btndata = [];
     //console.log(data[(Len-1)]);
     //判斷:除非點到INPUT與Input裡面有值，才做
     if(baninput.value === ''){return}
     //抓陣列最後一筆資料並將值塞到對應的地方
-    for (var i = 0; i < Len; i++) {
+    for (let i = 0; i < Len; i++) {
       btndata = data[(Len-1)]
       resultbtn.value = btndata.showBMI;
       resulttext.textContent = btndata.level;
       resultbtn.style.background="rgba(255,255,255,0)";
       inputimg.innerHTML = '<img src="images/icons_loop.png" alt=""><div class="bmitext">BMI</div>'
     //先將資料inner到img裡面才能給下面抓，這邊宣告不能拿到上面
-      var changetext = document.querySelector('.bmitext');
-      console.log(btndata);
+      let changetext = document.querySelector('.bmitext');
       //修改樣式
       switch(btndata.level){
         case "過輕":
@@ -151,5 +157,4 @@ function updatepage(){
   }
 
 resultbtn.addEventListener('click',insertdata,false);
-updatepage();
-resultbtn.addEventListener('click',refreshbtn,false);
+//resultbtn.addEventListener('click',refreshbtn,false);
